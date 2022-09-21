304ab2368fd7a4e7a7e37370dc3df58b7aa7f097
Alexander Bakhtin <alexander.bakhtin@tuni.fi>
2022-09-21 13:22:15 +0300

    add: map_file_and_label()

--- scripts.py
+++ scripts.py
@@ -10,6 +10,23 @@ def load_labels(file):
     file.close()
     return labels
 
+def map_file_and_label(file):
+    file = open(file, 'r')
+    file_to_label = dict()
+    label_to_file = dict()
+    for line in file:
+        parts = line.split('\t')
+        filename = parts[0]
+        label = parts[1].removesuffix('\n')
+        if parts[0] == 'filename': continue
+        s = file_to_label.setdefault(filename, set())
+        s.add(label)
+        s = label_to_file.setdefault(label, set())
+        s.add(file)
+    file.close()
+    return file_to_label, label_to_file
 
 if __name__ == '__main__':
     labels = load_labels(os.path.join('src', 'class_labels.tsv'))
+    se_ftl, se_ltf = map_file_and_label(os.path.join('src', 'audioset_strong_eval.tsv'))
+    st_ftl, st_ltf = map_file_and_label(os.path.join('src', 'audioset_strong_train.tsv'))
